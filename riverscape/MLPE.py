f1d5cffc9cd214ae9fbcddcd0235d744c9a72916
Tyler Chafin <tkchafin@uark.edu>
2020-12-22 14:57:38 -0700

    load R functions directly

--- riverscape/MLPE.py
+++ riverscape/MLPE.py
@@ -36,9 +36,44 @@ def MLPE_R(X, Y, scale=True):
 	ID["y"] = y
 	#print(ID)
 	
+	stuff="""
+library(Matrix)
+library(lme4)
+library(MuMIn)
+
+MLPE <- function(ID, ZZ, REML=FALSE) {
+  #print(ZZ)
+	ZZ<-Matrix(data.matrix(ZZ), sparse=TRUE)
+	#print(ZZ)
+	# prepare model
+  mod_list <- list('full'=lme4:::lFormula(y ~ x + (1 | pop1), data = ID, REML = FALSE),
+                   "null"=lme4:::lFormula(y ~ 1 + (1 | pop1), data = ID, REML = FALSE))
+  MODs <- lapply(mod_list, function(x) {
+    x$reTrms$Zt <- ZZ
+    # fit model
+    dfun <- do.call(lme4:::mkLmerDevfun, x)
+    opt <- lme4:::optimizeLmer(dfun)
+    MOD <- (lme4:::mkMerMod(environment(dfun), opt, x$reTrms, fr = x$fr))
+    #print(MOD)
+  })
+	models<-c("full")
+	full<-summary(MODs$full)
+	null<-summary(MODs$null)
+	loglik<-c(full$logLik[[1]])
+	r2<-c(MuMIn:::r.squaredGLMM(MODs$full)[[1]])
+	aic<-c(full$AICtab[[1]])
+	deltaAIC<-c(null$AICtab[[1]]-full$AICtab[[1]])
+	df<-data.frame(loglik, r2, -1*aic, deltaAIC)
+	colnames(df) <- c("loglik", "r2m", "aic", "delta_aic_null")
+  # return model
+  return(df)
+}
+	"""
+	
 	r=ro.r
 	r['options'](warn=-1)
-	r['source']("riverscape/resistanceGA_MLPE.R")
+	r(stuff)
+	#r['source']("riverscape/resistanceGA_MLPE.R")
 		
 	mlpe=ro.globalenv['MLPE']
 	with localconverter(ro.default_converter + pandas2ri.converter):
